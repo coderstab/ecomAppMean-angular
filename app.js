b658fd152ecdf38133473421389034287e388cc1
Soumya Ranjan <soumyaranjan091r@gmail.com>
2022-11-25 20:47:16 +0530

    backend setup start-nodemon install - expressjs install - env file setup - dummy get,post method implement

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express(); 
+const morgan = require('morgan');
+
+require('dotenv/config');
+const api = process.env.API_URL;
+
+//middleware 
+app.use(express.json());
+app.use(morgan('tiny')); 
+
+app.get(`${api}/products`, (req,res) =>{
+    const product = {
+        id:1,
+        name:'hair dresser',
+        image:'image_url'
+    }
+    res.send(product);
+} )
+
+app.post(`${api}/products`, (req,res) =>{
+    const newProduct = req.body;
+    console.log(newProduct);  
+    res.send(newProduct);
+} )
+
+app.listen(3000, () => {
+    console.log(api)
+    console.log('server is running http://localhost:3000')
+})
