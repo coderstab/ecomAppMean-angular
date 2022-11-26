1c714bd9e88ed54c8efa1914b7da5b0b61d22201
Soumya Ranjan <soumyaranjan091r@gmail.com>
2022-11-26 19:49:09 +0530

    connect with mongo db and create a post function with db

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express(); 
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 require('dotenv/config');
-const api = process.env.API_URL;
+const api = '/api/v1';
 
 //middleware 
 app.use(express.json());
 app.use(morgan('tiny')); 
+
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: Number
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+
 
 app.get(`${api}/products`, (req,res) =>{
     const product = {
@@ -19,10 +30,35 @@ app.get(`${api}/products`, (req,res) =>{
 } )
 
 app.post(`${api}/products`, (req,res) =>{
-    const newProduct = req.body;
-    console.log(newProduct);  
-    res.send(newProduct);
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
+
+    product.save().then((createdProduct =>{
+        res.status(201).json(createdProduct)
+
+    })).catch((err)=>{
+        res.status(500).json({
+            error:err,
+            success: false
+        })
+    })
+    // const newProduct = req.body;
+    // console.log(newProduct);  
+    // res.send(newProduct);
 } )
+const conn = 'mongodb+srv://mongomean:MongoMan123@mongomean.xccqhaf.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(conn,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    dbName: 'eshop-database'
+}).then(()=>{
+    console.log('databse connected');
+}).catch((err)=>{
+    console.log(err)
+})
 
 app.listen(3000, () => {
     console.log(api)
