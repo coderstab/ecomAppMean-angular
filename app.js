c81acdad1e4ed3f40976c0898a437063e5ee60ef
Soumya Ranjan <soumyaranjan091r@gmail.com>
2022-11-28 17:37:47 +0530

    create route and model for api

--- app.js
+++ app.js
@@ -5,55 +5,23 @@ const mongoose = require('mongoose');
 require('dotenv/config');
 
 const api = process.env.API_URL;
+const productsRouter = require('./routers/products');
+const usersRouter = require('./routers/users')
 
 //middleware 
 app.use(express.json());
-app.use(morgan('tiny')); 
-
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: Number
-})
-
-const Product = mongoose.model('Product', productSchema);
+app.use(morgan('tiny'));
 
 
+// Routers
+app.use(`${api}/products`,productsRouter);
+app.use(`${api}/users`,usersRouter);
 
-app.get(`${api}/products`, (req,res) =>{
-    const product = {
-        id:1,
-        name:'hair dresser',
-        image:'image_url'
-    }
-    res.send(product);
-} )
-
-app.post(`${api}/products`, (req,res) =>{
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-
-    product.save().then((createdProduct =>{
-        res.status(201).json(createdProduct)
-
-    })).catch((err)=>{
-        res.status(500).json({
-            error:err,
-            success: false
-        })
-    })
-    // const newProduct = req.body;
-    // console.log(newProduct);  
-    // res.send(newProduct);
-} )
 const conn = process.env.MONGO_CONN;
 mongoose.connect(conn,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
-    dbName: 'eshop-database'
+    dbName: 'nodeman'
 }).then(()=>{
     console.log('databse connected');
 }).catch((err)=>{
